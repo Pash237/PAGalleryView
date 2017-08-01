9deb84755f9a59366d94e27c7f6ebaf8d18426b4
Pavel Alexeev <pasha.alexeev@gmail.com>
2017-08-01 17:31:20 +0300

    New example images

--- Example/GalleryViewExample/ViewController.m
+++ Example/GalleryViewExample/ViewController.m
@@ -20,30 +20,21 @@
 {
     [super viewDidLoad];
 
-//
-//  Local images:
-//
-//	NSArray *images = @[
-//			[[NSBundle mainBundle] URLForResource:@"leaf_1" withExtension:@"jpg"],
-//			[[NSBundle mainBundle] URLForResource:@"leaf_2" withExtension:@"jpg"],
-//			[[NSBundle mainBundle] URLForResource:@"leaf_3" withExtension:@"jpg"],
-//			[[NSBundle mainBundle] URLForResource:@"leaf_4" withExtension:@"jpg"],
-//			[[NSBundle mainBundle] URLForResource:@"leaf_5" withExtension:@"jpg"],
-//			[[NSBundle mainBundle] URLForResource:@"leaf_6" withExtension:@"jpg"],
-//	];
+    NSArray *images = @[
+                [NSURL URLWithString:[NSString stringWithFormat:@"https://images.pexels.com/photos/6832/waterfall-beauty-lets-explore-lets-get-lost.jpg?h=150&%d", (int)arc4random_uniform(10000)]],
+                [NSURL URLWithString:[NSString stringWithFormat:@"https://images.pexels.com/photos/237301/pexels-photo-237301.jpeg?h=150&%d", (int)arc4random_uniform(10000)]],
+                [NSURL URLWithString:[NSString stringWithFormat:@"https://images.pexels.com/photos/210186/pexels-photo-210186.jpeg?h=150&%d", (int)arc4random_uniform(10000)]],
+                [NSURL URLWithString:[NSString stringWithFormat:@"https://images.pexels.com/photos/62627/niagara-cases-water-waterfall-62627.jpeg?h=150&%d", (int)arc4random_uniform(10000)]]
+    ];
+    
+    NSArray *fullScreenImages = @[
+                [NSURL URLWithString:[NSString stringWithFormat:@"https://images.pexels.com/photos/6832/waterfall-beauty-lets-explore-lets-get-lost.jpg?w=940&h=650&%d", (int)arc4random_uniform(10000)]],
+                [NSURL URLWithString:[NSString stringWithFormat:@"https://images.pexels.com/photos/237301/pexels-photo-237301.jpeg?w=940&h=650&%d", (int)arc4random_uniform(10000)]],
+                [NSURL URLWithString:[NSString stringWithFormat:@"https://images.pexels.com/photos/210186/pexels-photo-210186.jpeg?w=940&h=650&%d", (int)arc4random_uniform(10000)]],
+                [NSURL URLWithString:[NSString stringWithFormat:@"https://images.pexels.com/photos/62627/niagara-cases-water-waterfall-62627.jpeg?w=940&h=650&%d", (int)arc4random_uniform(10000)]]
+    ];
 
-	NSArray *images = @[
-			[NSURL URLWithString:[NSString stringWithFormat:@"http://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Big_Sur_June_2008.jpg/250px-Big_Sur_June_2008.jpg?%d", arc4random_uniform(10000)]],
-			[NSURL URLWithString:[NSString stringWithFormat:@"http://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/McWay_Falls_04-17-2009.jpg/220px-McWay_Falls_04-17-2009.jpg?%d", arc4random_uniform(10000)]],
-			[NSURL URLWithString:[NSString stringWithFormat:@"http://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Bixby_Creek_Bridge_1932..jpg/220px-Bixby_Creek_Bridge_1932..jpg?%d", arc4random_uniform(10000)]]
-	];
-
-	NSArray *fullScreenImages = @[
-			[NSURL URLWithString:[NSString stringWithFormat:@"http://upload.wikimedia.org/wikipedia/commons/6/6b/Big_Sur_June_2008.jpg?%d", arc4random_uniform(10000)]],
-			[NSURL URLWithString:[NSString stringWithFormat:@"http://upload.wikimedia.org/wikipedia/commons/e/e5/McWay_Falls_04-17-2009.jpg?%d", arc4random_uniform(10000)]],
-			[NSURL URLWithString:[NSString stringWithFormat:@"http://upload.wikimedia.org/wikipedia/commons/5/50/Bixby_Creek_Bridge_1932..jpg?%d", arc4random_uniform(10000)]]
-	];
-
+    
 	self.galleryView.imageURLs = images;
 	self.galleryView.fullScreenImageURLs = fullScreenImages;
 	self.galleryView.errorImage = [UIImage imageNamed:@"error_image.png"];
